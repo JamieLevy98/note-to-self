32b5fd5ed5de73df17dee223a9d792e81e14547d
JamieLevy98 <jamielevy@developersinstitute.co.nz>
2020-11-16 17:32:07 +1300

    created basic crud routes for note operations.

--- db/sql/Database.sql
+++ db/sql/Database.sql
@@ -1,10 +1,15 @@
 CREATE DATABASE note_to_self;
+
 \c note_to_self;
+
 CREATE TABLE notes(
   note_id SERIAL PRIMARY KEY,
   title VARCHAR(50),
   colour VARCHAR(32)
 );
+
+\c note_to_self;
+
 CREATE TABLE note_item(
   note_item_id SERIAL PRIMARY KEY,
   noteid_id INT,
--- server/routes/noteRouter.js
+++ server/routes/noteRouter.js
@@ -19,7 +19,7 @@ noteRouter.post("/", async (req, res, next) => {
 //Add a single item to a note.
 noteRouter.post("/add", async (req, res, next) => {
   try {
-    const {} = req.body;
+    const { noteid_id, description, isChecked } = req.body;
     const newNoteEntry = await pool.query(
       "INSERT INTO note_item (noteid_id, description, isChecked) VALUES($1, $2, $3) RETURNING *",
       [noteid_id, description, isChecked]
@@ -40,28 +40,44 @@ noteRouter.get("/", async (req, res, next) => {
   }
 });
 
-//Get a single note by its id
-//Maybe use this to load the contents of a note?
-//ALSO possibly use put route to update a list item
+//Get a single notes content by its id
 noteRouter.get("/:id", async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const noteContent = await pool.query(
+      "SELECT description, ischecked FROM note_item WHERE noteid_id = $1",
+      [id]
+    );
+    return res.status(200).json(noteContent.rows);
   } catch (e) {
     next(e);
   }
 });
 
-//Update a list items checked status from false to true/ vi ve
+//Update a list items checked status true/false
 noteRouter.put("/item/:id", async (req, res, next) => {
-try {
-  
-} catch (e) {
-  next(e);
-}
+  try {
+    const { id } = req.params;
+    const newStatus = await pool.query(
+      "UPDATE note_item SET ischecked = NOT ischecked WHERE note_item_id = $1 RETURNING *",
+      [id]
+    );
+    return res.status(200).json(newStatus.rows[0]);
+  } catch (e) {
+    next(e);
+  }
 });
 
 //Put route to hopefully update a note title.
 noteRouter.put("/:id", async (req, res, next) => {
   try {
+    const { title } = req.body;
+    const { id } = req.params;
+    const updatedNote = await pool.query(
+      "UPDATE notes SET title = $1 WHERE note_id = $2 RETURNING *",
+      [title, id]
+    );
+    return res.status(200).json(updatedNote.rows[0]);
   } catch (e) {
     next(e);
   }
@@ -70,6 +86,10 @@ noteRouter.put("/:id", async (req, res, next) => {
 //Delete a note
 noteRouter.delete("/:id", async (req, res, next) => {
   try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM note_item WHERE noteid_id = $1", [id]);
+    await pool.query("DELETE FROM notes WHERE note_id = $1", [id]);
+    return res.status(200).json({ success: "note deleted" });
   } catch (e) {
     next(e);
   }
@@ -78,9 +98,11 @@ noteRouter.delete("/:id", async (req, res, next) => {
 //delete a list item
 noteRouter.delete("/item/:id", async (req, res, next) => {
   try {
-    
+    const { id } = req.params;
+    await pool.query("DELETE FROM note_item WHERE note_item_id = $1", [id]);
+    return res.status(200).json({ success: "note item deleted" });
   } catch (e) {
     next(e);
   }
-})
+});
 module.exports = noteRouter;
